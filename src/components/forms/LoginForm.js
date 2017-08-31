139f2f3e3a7a124819f955fee33022c64ca6df45
TroublemakerSt <bugandcode@gmail.com>
2017-08-31 18:14:19 +0300

    Errors was added when one of form stay blank

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import Validator from 'validator';
+import InlineError from '../messages/InlineError';
+
+class LoginForm extends React.Component {
+  state = {
+    data: {
+      email: '',
+      password: '',
+    },
+    loading: false,
+    errors: {},
+  };
+
+  onChange = e =>
+    this.setState({ ...this.state.data, [e.target.name]: e.target.value });
+
+  onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+  };
+
+  validate = data => {
+    const errors = {};
+    if (!Validator.isEmail(data.email)) errors.email = 'Invalid email';
+    if (!data.password) errors.password = "Cant't be blank";
+    return errors;
+  };
+
+  render() {
+    const { data, errors } = this.state;
+    return (
+      <Form onSubmit={this.onSubmit}>
+        <Form.Field>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your email adress"
+            value={data.email}
+            onChange={this.onChange}
+          />
+          {errors.email && <InlineError text={errors.email} />}
+        </Form.Field>
+        <Form.Field>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Your password"
+            value={data.password}
+            onChange={this.onChange}
+          />
+          {errors.password && <InlineError text={errors.password} />}
+        </Form.Field>
+        <Button primary>Login</Button>
+      </Form>
+    );
+  }
+}
+
+export default LoginForm;
